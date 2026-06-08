621c49da659a9a4d1c233e9f4fced2aeaf279b74
EHMD28 <ehmdiagne0530@gmail.com>
2026-06-08 19:12:24 -0400

    added all motors

--- src/main.cpp
+++ src/main.cpp
@@ -16,6 +16,7 @@ class MotorChannel {
         uint8_t in2_pin;
         uint8_t pwm_channel;
         bool is_reversed;
+        
 
         void drive_cw(uint8_t duty_cycle) {
             digitalWrite(in1_pin, HIGH);
@@ -28,7 +29,7 @@ class MotorChannel {
             digitalWrite(in2_pin, HIGH);
             ledcWrite(pwm_channel, duty_cycle);
         }
-        
+
     public:
         MotorChannel(uint8_t in1_pin, uint8_t in2_pin, uint8_t pwm_pin, uint8_t channel, bool is_reversed) {
             this->in1_pin = in1_pin;
@@ -36,25 +37,14 @@ class MotorChannel {
             this->pwm_channel = channel;
             this->is_reversed = is_reversed;
 
+            pinMode(in1_pin, OUTPUT);
+            pinMode(in2_pin, OUTPUT);
+            pinMode(pwm_pin, OUTPUT);
+
             ledcAttachPin(pwm_pin, channel);
             ledcSetup(channel, MOTOR_PWM_FREQUENCY, MOTOR_RESOLUTION);
 
             stopMotor();
-        }
-
-        /**
-         * @param speed The speed and direction to drive the motor. Values: [-255, 255].
-         */
-        void driveMotor(int16_t speed) {
-            speed *= is_reversed ? -1 : 1;
-            // CCW Rotation is positive
-            if (speed > 0) {
-                drive_ccw(speed);
-            } else if (speed < 0) {
-                drive_cw(-speed);
-            } else {
-                stopMotor();
-            }
         }
 
         /**
@@ -65,52 +55,80 @@ class MotorChannel {
             digitalWrite(in2_pin, LOW);
             ledcWrite(pwm_channel, 0);
         }
+        
+        /**
+         * @param speed The speed and direction to drive the motor. Value: [-255, 255].
+         */
+        void driveMotor(int16_t speed) {
+            speed *= is_reversed ? -1 : 1;
+            /* CCW rotation is positive */
+            if (speed > 0) {
+                drive_ccw(speed);
+            } else if (speed < 0) {
+                drive_cw(-speed);
+            } else {
+                stopMotor();
+            }
+        }
 };
 
 /* -------- Status LED -------- */
-constexpr int STATUS_LED_PIN = 2;
-constexpr int SHORT_DELAY = 2000;
+constexpr uint8_t STATUS_LED_PIN = 2;
+constexpr uint16_t SHORT_DELAY = 2000;
 
-/* -------- Motor Driver 1 (Back Left and Back Right) -------- */
-constexpr int DRIVER1_BIN2_PIN = 4;
-constexpr int DRIVER1_BIN1_PIN = 13;
-constexpr int DRIVER1_AIN2_PIN = 14;
-constexpr int DRIVER1_AIN1_PIN = 18;
-constexpr int DRIVER1_PWMB_PIN = 19;
-constexpr int DRIVER1_PWMA_PIN = 21;
+/* -------- Motor Driver 1 (Left Side) -------- */
+constexpr uint8_t DRIVER1_BIN2_PIN = 4;
+constexpr uint8_t DRIVER1_BIN1_PIN = 13;
+constexpr uint8_t DRIVER1_AIN2_PIN = 14;
+constexpr uint8_t DRIVER1_AIN1_PIN = 18;
+constexpr uint8_t DRIVER1_PWMB_PIN = 19;
+constexpr uint8_t DRIVER1_PWMA_PIN = 21;
 
-/* -------- Motor Driver 2 -------- */
-constexpr int DRIVER2_BIN2_PIN = 32;
-constexpr int DRIVER2_BIN1_PIN = 27;
-constexpr int DRIVER2_AIN2_PIN = 26;
-constexpr int DRIVER2_AIN1_PIN = 25;
-constexpr int DRIVER2_PWMB_PIN = 23;
-constexpr int DRIVER2_PWMA_PIN = 22;
+/* -------- Motor Driver 2 (Right Side) -------- */
+constexpr uint8_t DRIVER2_BIN2_PIN = 32;
+constexpr uint8_t DRIVER2_BIN1_PIN = 27;
+constexpr uint8_t DRIVER2_AIN2_PIN = 26;
+constexpr uint8_t DRIVER2_AIN1_PIN = 25;
+constexpr uint8_t DRIVER2_PWMB_PIN = 23;
+constexpr uint8_t DRIVER2_PWMA_PIN = 22;
 
 /* -------- Motor PWM Channels -------- */
-constexpr int FRONT_LEFT_CHANNEL = 1;
+constexpr uint8_t FRONT_LEFT_CHANNEL = 1;
+constexpr uint8_t BACK_LEFT_CHANNEL = 2;
+constexpr uint8_t FRONT_RIGHT_CHANNEL = 3;
+constexpr uint8_t BACK_RIGHT_CHANNEL = 4;
 
-/* -------- Configuration Constants -------- */
-constexpr int STBY_PIN = 33;  // This pin is unconnected becuase STBY is always pulled high.
-constexpr int IS_REVERSED_FALSE = 1;
-constexpr int IS_REVERSED_TRUE = -1;
-constexpr int PWM_FREQUENCY = 5000;
-constexpr int PWM_RESOLUTION = 8;
-constexpr int MAX_PWM = 255;
-
-/* Drivetrain */
+/* -------- Drivetrain -------- */
 /* The last argument to each constructor is whether or not the motor is reversed. */
 MotorChannel front_left = MotorChannel(DRIVER1_AIN1_PIN, DRIVER1_AIN2_PIN, DRIVER1_PWMA_PIN,
-                                        FRONT_LEFT_CHANNEL, false);
-
+                                        FRONT_LEFT_CHANNEL, true);
+MotorChannel back_left = MotorChannel(DRIVER1_BIN1_PIN, DRIVER1_BIN2_PIN, DRIVER1_PWMB_PIN,
+                                        BACK_LEFT_CHANNEL, true);
+MotorChannel front_right = MotorChannel(DRIVER2_AIN1_PIN, DRIVER2_AIN2_PIN, DRIVER2_PWMA_PIN,
+                                        FRONT_RIGHT_CHANNEL, false);
+MotorChannel back_right = MotorChannel(DRIVER2_BIN1_PIN, DRIVER2_BIN2_PIN, DRIVER2_PWMB_PIN,
+                                        BACK_RIGHT_CHANNEL, true);
+                                        
 void setup() {
+    pinMode(STATUS_LED_PIN, OUTPUT);
+    // digitalWrite(STATUS_LED_PIN, HIGH);
+}
+
+void test_drive() {
+    front_left.driveMotor(100);
+    delay(SHORT_DELAY);
+    front_left.stopMotor();
+    front_right.driveMotor(100);
+    delay(SHORT_DELAY);
+    front_right.stopMotor();
+    back_right.driveMotor(100);
+    delay(SHORT_DELAY);
+    back_right.stopMotor();
+    back_left.driveMotor(100);
+    delay(SHORT_DELAY);
+    back_left.stopMotor();
 }
 
 void loop() {
-    front_left.driveMotor(100);
-    delay(2000);
-    front_left.stopMotor();
-    delay(2000);
-    front_left.driveMotor(-100);
-    delay(2000);
+    test_drive();
 }
